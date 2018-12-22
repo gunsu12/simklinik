9b32b8ee054f29d99e9b2996d2d9a3fe523dd38f
mbx92 <38708861+mbx92@users.noreply.github.com>
2018-12-22 11:23:04 +0800

    Update v_registrasi.php

--- application/views/Modules/front_office/v_registrasi.php
+++ application/views/Modules/front_office/v_registrasi.php
@@ -39,103 +39,77 @@
 								    <div class="col-sm-2">
 								            <button type="button" class="btn btn-raised g-bg-cyan btn-block btn-lg waves-effect" data-toggle="modal" data-target="#">Kode Booking</button>
 								    </div>
-								    <div class="col-sm-2">
+								    <div class="col-sm-4">
 								        <div class="form-group">
 								            <div class="form-line">
-								                <input type="text" name="kode_booking" class="form-control" placeholder="Kode Booking" id="kode_booking">
-								            </div>
-								        </div>
-								    </div>
-								    <div class="col-sm-3">
-								        <div class="form-group">
-								            <div class="form-line">
-								                <input type="text" name="no_register" class="form-control" placeholder="No Register" id="no_register">
-								            </div>
-								        </div>
-								    </div>
-								    <div class="col-sm-3">
-								        <div class="form-group">
-								            <div class="form-line">
-								                <input type="text" name="rekam_medis" class="form-control" placeholder="No Rekam Medis" id="rekam_medis">
-								            </div>
-								        </div>
-								    </div>
-								</div>
-								<div class="row clearfix">
-									<div class="col-sm-3">
-								        <div class="form-group">
-								            <div class="form-line">
-								                <input type="text" name="layanan" class="form-control" placeholder="Layanan" id="layanan">
+								                <input type="text" name="kode_booking" class="form-control" placeholder="Kode Booking" id="kode_booking" disabled="">
 								            </div>
 								        </div>
 								    </div>
 								    <div class="col-sm-4">
 								        <div class="form-group">
 								            <div class="form-line">
-								                <input type="text" name="dr_spesialist" class="form-control" placeholder="Dokter Spesialist" id="dr_spesialist">
+								                <input type="text" name="no_register" class="form-control" placeholder="No Register" id="no_register">
 								            </div>
 								        </div>
 								    </div>
-								    <div class="col-sm-3">
-								        <div class="form-group"  data-toggle="modal" data-target="#test">
-								            <div class="form-line">
-								                <input type="text" name="jaminan" class="form-control" placeholder="Jaminan" id="jaminan">
-								            </div>
-								        </div>
-								    </div>
-								    <div class="col-sm-2">
+								</div>
+								<div class="row clearfix">
+									<div class="col-sm-2">
 								        <div class="form-group">
 								            <div class="form-line">
-								                <input type="text" name="admin" class="form-control" placeholder="Administrasi" id="admin">
-								            </div>
-								        </div>
-								    </div>
-								    <!-- <div class="col-sm-3">
-								        <div class="form-group">
-								            <div class="form-line">
-								                <input type="text" name="dr_tgl_lhr" class="datepicker form-control" placeholder="Tanggal Lahir" id="datetime" id="dr_tgl_lhr">
-								            </div>
-								        </div>
-								    </div> -->
-								    <!-- <div class="col-sm-3">
-								        <div class="form-group drop-custum">
-								            <select class="form-control show-tick">
-								                <option value="">-- Jenis Kelamin --</option>
-								                <option value="10">Male</option>
-								                <option value="20">Female</option>
-								            </select>
-								        </div>
-								    </div> -->
-								    <div class="col-sm-3">
-								        <div class="form-group">
-								            <div class="form-line">
-								                <input type="text" name="dr_spesialist" class="form-control" placeholder="Spesialist" id="dr_spesialist">
-								            </div>
-								        </div>
-								    </div>
-								    <div class="col-sm-3">
-								        <div class="form-group">
-								            <div class="form-line">
-								                <input type="text" name="dr_tlp" class="form-control" placeholder="No Telepon" id="dr_tlp">
-								            </div>
-								        </div>
-								    </div>
-								   <!--  <div class="col-sm-6">
-								        <div class="form-group">
-								            <div class="form-line">
-								                <input type="text" name="dr_alamat" class="form-control" placeholder="Alamat" id="dr_alamat">
+								                <input type="text" name="rekam_medis" class="form-control" placeholder="No Rekam Medis" id="rekam_medis">
 								            </div>
 								        </div>
 								    </div>
 								    <div class="col-sm-6">
 								        <div class="form-group">
 								            <div class="form-line">
-								                <input type="text" name="dr_email" class="form-control" placeholder="Email" id="dr_email">
+								                <input type="text" name="nama_pasien" class="form-control" placeholder="Nama Pasien" id="nama_pasien">
 								            </div>
 								        </div>
-								    </div> -->
+								    </div>
+								    <div class="col-sm-4">
+								        <div class="form-group">
+								            <div class="form-line">
+								                <input type="text" name="jenis_kunjungan" class="form-control" placeholder="Jenis Kunjungan" id="jenis_kunjungan">
+								            </div>
+								        </div>
+								    </div>
+								</div>
+								<div class="row clearfix">
+									<div class="col-sm-3">
+								        <div class="form-group" data-toggle="modal" data-target="#daftarlayanan">
+								            <div class="form-line">
+								                <input type="text" name="layanan" class="form-control" placeholder="Layanan" id="layanan">
+								            </div>
+								        </div>
+								    </div>
+								    <div class="col-sm-4">
+								        <div class="form-group" data-toggle="modal" data-target="#daftardokter">
+								            <div class="form-line">
+								                <input type="text" name="dr_spesialist" class="form-control" placeholder="Dokter Spesialist" id="dr_spesialist">
+								            </div>
+								        </div>
+								    </div>
+								    <div class="col-sm-3">
+								        <div class="form-group" data-toggle="modal" data-target="#daftarjaminan">
+								            <div class="form-line">
+								                <input type="text" name="jaminan" class="form-control" placeholder="Jaminan" id="jaminan">
+								            </div>
+								        </div>
+								    </div>
+								    <div class="col-sm-2">
+								        <div class="form-group" data-toggle="modal" data-target="#daftaradmin">
+								            <div class="form-line">
+								                <input type="text" name="admin" class="form-control" placeholder="Administrasi" id="admin">
+								            </div>
+								        </div>
+								    </div>
+								    
 								    <div class="col-sm-12">
 								        <button id="submit" class="btn btn-raised g-bg-cyan btn-lg">Simpan</button>
+								        <button id="datasosial" class="btn btn-raised bg-grey waves-effect btn-lg" data-toggle="modal" data-target="#datasosialform">Data Sosial Pasien</button>
 								    </div>
 								</div>
                         	<!-- </form>		 -->
@@ -185,27 +159,25 @@
 		</div>
     </div>
 
-	<!-- caridokterModal -->
-	<div class="modal fade" id="test" role="dialog" data-backdrop="false">
-	    <div class="modal-dialog modal-lg">
+    <!-- daftarLayanan -->
+	<div class="modal fade" id="daftarlayanan" role="dialog" data-backdrop="false">
+	    <div class="modal-dialog modal-md">
 	        <div class="modal-content">
 	            <div class="modal-header">
-	                <h4 class="modal-title">Nama Dokter</h4>
+	                <h4 class="modal-title">Daftar Layanan</h4>
 	            </div>
 	            <div class="modal-body"> 
 	            	<table class="table table-hover">
                 		<tr>
                 			<th style="width:5%; text-align:center;">No</th>
-                			<th>Kode Dokter</th>
-                			<th>Nama Dokter</th>
-                			<th>Spesialist</th>
+                			<th>Kode Layanan</th>
+                			<th>Nama Layanan</th>
                 			<th style="width:5%; text-align:center;">Action</th>
                 		</tr>
                 		<tr>
                 			<td style="width:5%; text-align:center;">1</td>
-                			<td>DP0001</td>
-                			<td>Pande Wira Andika</td>
-                			<td>Komputer</td>
+                			<td>LY0001</td>
+                			<td>Poli Umum</td>
                 			<td style="text-align:center;"><a href="javascript:void(0);" class="edit"><i class="zmdi zmdi-check"></i></a></td>
                 		</tr>
                     </table>
@@ -217,6 +189,187 @@
 	        </div>
 	    </div>
 	</div>
+	<!-- daftarLayanan -->
+
+	<!-- daftarDokter -->
+	<div class="modal fade" id="daftardokter" role="dialog" data-backdrop="false">
+	    <div class="modal-dialog modal-lg">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h4 class="modal-title">Daftar Dokter</h4>
+	            </div>
+	            <div class="modal-body"> 
+	            	<table class="table table-hover">
+                		<tr>
+                			<th style="width:5%; text-align:center;">No</th>
+                			<th style="width:15%;"">Kode Dokter</th>
+                			<th>Nama Dokter</th>
+                			<th style="width:5%; text-align:center;">Action</th>
+                		</tr>
+                		<tr>
+                			<td style="width:5%; text-align:center;">1</td>
+                			<td>DP0001</td>
+                			<td>dr. Pande</td>
+                			<td style="text-align:center;"><a href="javascript:void(0);" class="edit"><i class="zmdi zmdi-check"></i></a></td>
+                		</tr>
+                    </table>
+                </div>
+	            <div class="modal-footer">
+	                <!-- <button type="button" class="btn btn-link waves-effect">SAVE CHANGES</button> -->
+	                <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CLOSE</button>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+	<!-- daftarDokter -->
+
+	<!-- daftarJaminan -->
+	<div class="modal fade" id="daftarjaminan" role="dialog" data-backdrop="false">
+	    <div class="modal-dialog modal-md">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h4 class="modal-title">Daftar Jaminan</h4>
+	            </div>
+	            <div class="modal-body"> 
+	            	<table class="table table-hover">
+                		<tr>
+                			<th style="width:5%; text-align:center;">No</th>
+                			<th>Kode Jaminan</th>
+                			<th>Nama Jaminan</th>
+                			<th style="width:5%; text-align:center;">Action</th>
+                		</tr>
+                		<tr>
+                			<td style="width:5%; text-align:center;">1</td>
+                			<td>JM0001</td>
+                			<td>Prudential</td>
+                			<td style="text-align:center;"><a href="javascript:void(0);" class="edit"><i class="zmdi zmdi-check"></i></a></td>
+                		</tr>
+                    </table>
+                </div>
+	            <div class="modal-footer">
+	                <!-- <button type="button" class="btn btn-link waves-effect">SAVE CHANGES</button> -->
+	                <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CLOSE</button>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+	<!-- daftarJaminan -->
+
+	<!-- daftarAdmin -->
+	<div class="modal fade" id="daftaradmin" role="dialog" data-backdrop="false">
+	    <div class="modal-dialog modal-md">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h4 class="modal-title">Daftar Administrasi</h4>
+	            </div>
+	            <div class="modal-body"> 
+	            	<table class="table table-hover">
+                		<tr>
+                			<th style="width:5%; text-align:center;">No</th>
+                			<th>Kode Administrasi</th>
+                			<th>Nama Administrasi</th>
+                			<th style="width:5%; text-align:center;">Action</th>
+                		</tr>
+                		<tr>
+                			<td style="width:5%; text-align:center;">1</td>
+                			<td>AD0001</td>
+                			<td>Umum</td>
+                			<td style="text-align:center;"><a href="javascript:void(0);" class="edit"><i class="zmdi zmdi-check"></i></a></td>
+                		</tr>
+                    </table>
+                </div>
+	            <div class="modal-footer">
+	                <!-- <button type="button" class="btn btn-link waves-effect">SAVE CHANGES</button> -->
+	                <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CLOSE</button>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+	<!-- daftarAdmin -->
+
+	<!-- dataSosial -->
+	<div class="modal fade" id="datasosialform" role="dialog" data-backdrop="false">
+	    <div class="modal-dialog modal-lg">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h4 class="modal-title">Data Sosial Pasien</h4>
+	            </div>
+	            <div class="modal-body"> 
+	            	<ul class="nav nav-tabs">
+                        <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#data">Data Sosial</a></li>
+                        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#kerabat">Data Kerabat</a></li>
+                        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#alergi">Data Alergi</a></li>
+                        <!-- <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#settings">SETTINGS</a></li> -->
+                    </ul> 
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane in active" id="data">
+                        	<div class="row clearfix">
+			            		<div class="col-sm-3">
+							        <div class="form-group">
+							            <div class="form-line">
+							                <input type="text" name="dr_tgl_lhr" class="datepicker form-control" placeholder="Tanggal Lahir" id="date" id="dr_tgl_lhr">
+							            </div>
+							        </div>
+							    </div>
+								    <!-- <div class="col-sm-3">
+								        <div class="form-group drop-custum">
+								            <select class="form-control show-tick">
+								                <option value="">-- Jenis Kelamin --</option>
+								                <option value="10">Male</option>
+								                <option value="20">Female</option>
+								            </select>
+								        </div>
+								    </div> 
+								    <div class="col-sm-3">
+								        <div class="form-group">
+								            <div class="form-line">
+								                <input type="text" name="dr_spesialist" class="form-control" placeholder="Spesialist" id="dr_spesialist">
+								            </div>
+								        </div>
+								    </div>
+								    <div class="col-sm-3">
+								        <div class="form-group">
+								            <div class="form-line">
+								                <input type="text" name="dr_tlp" class="form-control" placeholder="No Telepon" id="dr_tlp">
+								            </div>
+								        </div>
+								    </div>-->
+								   <!--  <div class="col-sm-6">
+								        <div class="form-group">
+								            <div class="form-line">
+								                <input type="text" name="dr_alamat" class="form-control" placeholder="Alamat" id="dr_alamat">
+								            </div>
+								        </div>
+								    </div>
+								    <div class="col-sm-6">
+								        <div class="form-group">
+								            <div class="form-line">
+								                <input type="text" name="dr_email" class="form-control" placeholder="Email" id="dr_email">
+								            </div>
+								        </div>
+								    </div> -->
+	            			</div> 
+                        </div>
+
+                        <div role="tabpanel" class="tab-pane in active" id="kerabat">
+                        	
+                        </div>
+
+                        <div role="tabpanel" class="tab-pane in active" id="alergi">
+                        	
+                        </div>
+                    </div>
+	            	
+                </div>
+	            <div class="modal-footer">
+	                <!-- <button type="button" class="btn btn-link waves-effect">SAVE CHANGES</button> -->
+	                <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CLOSE</button>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+	<!-- dataSosial -->
 
 </section>
 
